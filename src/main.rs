764305ed2373c8ca67bce9b4e1cfb738eed33e70
Adrian Tombu <adrian@otso.fr>
2023-07-14 17:14:18 +0000

    feat: parallelize downloads

--- src/main.rs
+++ src/main.rs
@@ -6,14 +6,12 @@ mod session;
 use crate::connection::Connections;
 use crate::mailbox::Mailbox;
 use anyhow::{Context, Result};
+use rayon::prelude::*;
 use std::fs;
 
 fn main() -> Result<()> {
     let data = fs::read_to_string("config.toml").context("Could not read config.toml file")?;
+    let connections = &Connections::try_new(&data)?.connections;
 
-    for connection in &Connections::try_new(&data)?.connections {
-        Mailbox::fetch_all(connection)?;
-    }
-
-    Ok(())
+    connections.par_iter().try_for_each(Mailbox::fetch_all)
 }
